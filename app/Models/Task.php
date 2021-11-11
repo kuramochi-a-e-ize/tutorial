10aad19eb6918e33a56f11236a43e0affa4782fb
DESKTOP-UI3EA3C\倉持 <a.kuramochi@e-ize.jp>
2021-11-11 12:07:12 +0900

    TUT-004 モデルクラスにおけるリレーションの実装

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,6 +16,23 @@ class Task extends Model
         2 => [ 'label' => '着手中', 'class' => 'label-info' ],
         3 => [ 'label' => '完了', 'class' => '' ],
     ];
+
+    /**
+     * 状態を表すHTMLクラス
+     * @return string
+     */
+    public function getStatusClassAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
 
     /**
      * 状態のラベル
@@ -31,5 +49,15 @@ class Task extends Model
         }
 
         return self::STATUS[$status]['label'];
+    }
+
+    /**
+     * 整形した期限日
+     * @return string
+     */
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])
+            ->format('Y/m/d');
     }
 }
