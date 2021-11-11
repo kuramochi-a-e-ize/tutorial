d38cdb6496f621a4a82c60395e313eae88fb14a8
DESKTOP-UI3EA3C\倉持 <a.kuramochi@e-ize.jp>
2021-11-11 18:46:04 +0900

    TUT-008 認証機能の実装1

--- database/seeders/FoldersTableSeeder.php
+++ database/seeders/FoldersTableSeeder.php
@@ -15,14 +15,17 @@ class FoldersTableSeeder extends Seeder
      */
     public function run()
     {
-              $titles = ['プライベート', '仕事', '旅行'];
+        $user = DB::table('users')->first();
 
-              foreach ($titles as $title) {
-                  DB::table('folders')->insert([
-                      'title' => $title,
-                      'created_at' => Carbon::now(),
-                      'updated_at' => Carbon::now(),
-                  ]);
-              }
+        $titles = ['プライベート', '仕事', '旅行'];
+
+        foreach ($titles as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+                'user_id' => $user->id,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
