b7ec9743bde94750f6e4146ad6d7fea0eff343d0
DESKTOP-UI3EA3C\倉持 <a.kuramochi@e-ize.jp>
2021-11-11 13:03:55 +0900

    TUT-005 フォルダ作成機能の実装

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
 use App\Http\Controllers\OjisanController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,11 +16,17 @@ use App\Http\Controllers\OjisanController;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+// Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');  /* v6 */
+// Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');  /* v6 */
+// Route::post('/folders/{id}/tasks/create', 'TaskController@create');  /* v6 */
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
-//Route::get('/folders/{id}/tasks/plus', [TaskController::class, 'plus'], ['name' => 'tasks.plus']);
+Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
 
-Route::get('/folders/{id}/oji', [OjisanController::class, 'index'])->name('ojisans.index');
-Route::get('/folders/{id}/oji/plus', [OjisanController::class, 'plus'])->name('ojisans.plus');
+// Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');  /* v6 */
+// Route::post('/folders/create', 'FolderController@create');  /* v6 */
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [FolderController::class, 'create']);
+
+Route::get('/folders/oji', [OjisanController::class, 'index']);
+Route::get('/folders/oji/plus', [OjisanController::class, 'plus']);
