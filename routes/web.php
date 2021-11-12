7d778528c2d8c97bf9a0621da994d976078ffeda
DESKTOP-UI3EA3C\倉持 <a.kuramochi@e-ize.jp>
2021-11-12 12:12:39 +0900

    TUT-008 認証機能の実装2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\FolderController;
 use App\Http\Controllers\OjisanController;
@@ -16,27 +17,38 @@ use App\Http\Controllers\OjisanController;
 |
 */
 
-/* // v6
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'TaskController@create');
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
-*/
+//Auth::routes();
+Route::middleware(['auth:sanctum', 'verified'])->get('/home', function () {
+    return view('home');
+})->name('dashboard');
 
-Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
+Route::group(['middleware' => 'auth'], function() {
 
-Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+  Route::get('/', [HomeController::class, 'index'])->name('home');
 
-/* // v6
-Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-Route::post('/folders/create', 'FolderController@create');
-*/
-Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
-Route::post('/folders/create', [FolderController::class, 'create']);
+  /* // v6
+  Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+  Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+  Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+  */
 
-Route::get('/folders/oji', [OjisanController::class, 'index']);
-Route::get('/folders/oji/plus', [OjisanController::class, 'plus']);
+  Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+  Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
+
+  Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+
+  /* // v6
+  Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+  Route::post('/folders/create', 'FolderController@create');
+  */
+  Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+  Route::post('/folders/create', [FolderController::class, 'create']);
+
+  Route::get('/folders/oji', [OjisanController::class, 'index']);
+  Route::get('/folders/oji/plus', [OjisanController::class, 'plus']);
+
+});
